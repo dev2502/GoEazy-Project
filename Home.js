591b5fed3808d244b97afd758a4e0ddbea2f034f
Ayushi Chauhan <78093967+ayu3SHI0@users.noreply.github.com>
2023-03-22 23:16:27 +0530

    Update Home.js

--- Home.js
+++ Home.js
@@ -1,30 +1,41 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import logo from "./logo2.png";
 import bvlogo from "./bvlogo.jpg";
 import "./App.css";
-function Home()
-{
-  return(
-    <>
-    <div class="header" height="10%" width="10%">
-            <table class="logo" >
-             <tr> 
-              <td> <img src={logo} alt="Logo" height="10%" width="45%"/></td>
-              <td> <img src={bvlogo} alt="bvlogo" height="10%" width="50%" id="bvlogo"/>  </td>
-            </tr>
-            </table>
-    </div>
-    <div className="container1">
-    <table className="t1">
-      <tr> 
-        <td> <button   class="b1"> ADD </button></td>
-        <td> <button  class="b1"> Edit </button> </td>
-        <td> <button  class="b1" > Delete </button>  </td>
-        <td> <button  class="b1" >Search  </button></td>
-      </tr>
-    </table>
-    </div>
-    </>
-  )
+import "./home.css";
+
+function Home() {
+const navigate = useNavigate();
+
+const handleClick = () => navigate("/table");
+return (
+<>
+<div class="header" height="10%" width="10%">
+   <table class="logo">
+   <tr>
+<td>
+<img src={logo} alt="Logo" height="10%" width="45%" />
+</td>
+<td>
+<img src={bvlogo} alt="bvlogo" height="10%" width="50%" id="bvlogo"/>
+</td>
+</tr>
+</table>
+</div>
+<div className="container1">
+<table className="t1">
+<tr>
+<td>
+<button class="b1" onClick={handleClick}>Update </button>
+</td>
+<td>
+<button class="b1">Search </button>
+</td>
+</tr>
+</table>
+</div>
+</>
+);
 }
 export default Home;
