b80b7d2c93bef97fd76dbd8781638ca25fc49b01
Ayushi Chauhan <78093967+ayu3SHI0@users.noreply.github.com>
2023-03-22 23:17:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter , Routes , Route} from 'react-router-dom'
 import './index.css';
-import App from './App';
 import Home from './Home.js';
 import Table from './Table.js';
-import table from './table.css';
-import home from './home.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home/>
-    <Table/>
+  <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/table" element={<Table />} />
+    </Routes>
+  </BrowserRouter>
   </React.StrictMode>
 );
 
