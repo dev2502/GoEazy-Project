cf6efc18e2a49e1ecacc8b762a0bce504085b000
dev2502 <97377647+dev2502@users.noreply.github.com>
2023-03-23 18:36:42 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import logo from "./logo2.png";
 import bvlogo from "./bvlogo.jpg";
+import LoginPage from './Pages/LoginPage';
+import CustomerLogin from './Pages/CustomerLogin';
+import ShopKeeperLogin from './Pages/ShopKeeperLogin';
+import CreateNewAcc from'./Pages/CreateNewAcc';
 import "./App.css";
 import {useState} from "react";
+
 
 function App()
 {
   <>
-    </>
+    <LoginPage />
+    <CustomerLogin />
+    <ShopKeeperLogin />
+    <CreateNewAcc />
+  </>
 }
 export default App;
